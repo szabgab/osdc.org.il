64516003a8f9d677b9008389ac280f6bc284a7c7
Gabor Szabo <szabgab@gmail.com>
2013-01-03 18:04:23 +0200

    use bootstrap to create the main pages

---
+++ bin/generate.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use v5.10;
+use autodie;
+
+use Template;
+
+my @pages = glob "pages/*.pod";
+my $t = Template->new({
+	INCLUDE_PATH => 'tt',
+	POST_CHOMP   => 1,
+	EVAL_PERL    => 0,
+	});
+foreach my $p (@pages) {
+	open my $fh, '<', $p;
+	my %data = (body => '');
+	while (my $line = <$fh>) {
+		if ($line =~ /^=(\w+)\s+(.*?)\s*$/) {
+			$data{$1} = $2;
+			next;
+		}
+		$data{body} .= $line;
+	}
+	close $fh;
+
+	my $outfile = 'html/' . substr($p, 6, -3) . 'html';
+
+	$t->process('page.tt', \%data, $outfile) or die;
+}
+
